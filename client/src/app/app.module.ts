8f061dce803b4f77804322ed93d8c43fa0035427
tuncerb17 <tuncerb17@gmail.com>
2018-06-22 21:48:47 +0300

    first angular commit

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { BookService } from './shared/book/book.service';
+import { BookListComponent } from './book-list/book-list.component';
+
+import { AppComponent } from './app.component';
+
+import { MatButtonModule, MatCardModule, MatInputModule, MatListModule, MatToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatToolbarModule
+  ],
+  providers: [BookService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
