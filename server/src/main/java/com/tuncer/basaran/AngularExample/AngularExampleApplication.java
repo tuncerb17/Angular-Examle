2fd33b34111f48a2129b639ed6871b8c1c5dc08c
tuncerb17 <tuncerb17@gmail.com>
2018-06-22 21:47:51 +0300

    security oauth deleted.

--- server/src/main/java/com/tuncer/basaran/AngularExample/AngularExampleApplication.java
+++ server/src/main/java/com/tuncer/basaran/AngularExample/AngularExampleApplication.java
@@ -5,19 +5,10 @@ import com.tuncer.basaran.AngularExample.repositories.BookRepository;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import java.util.Collections;
 import java.util.stream.Stream;
 
-import org.springframework.core.Ordered;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@EnableResourceServer
 @SpringBootApplication
 public class AngularExampleApplication {
 
@@ -36,20 +27,5 @@ public class AngularExampleApplication {
 			});
 			repository.findAll().forEach(System.out::println);
 		};
-	}
-
-	@Bean
-	@SuppressWarnings("unchecked")
-	public FilterRegistrationBean simpleCorsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-		config.setAllowedMethods(Collections.singletonList("*"));
-		config.setAllowedHeaders(Collections.singletonList("*"));
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-		return bean;
 	}
 }
